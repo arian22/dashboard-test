62b5a4582ecd4b5a03f7f7b70cac4d539ea84363
arian22 <arian.mobarghei@gmail.com>
2026-02-16 20:45:28 -0700

    title

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,7 +12,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">آرین مبرقعی</h1>
+        <h1 className="text-base font-medium">ادمین</h1>
         <div className="mr-auto flex items-center gap-2">
           < ThemeSelector />
           <ModeToggle />
